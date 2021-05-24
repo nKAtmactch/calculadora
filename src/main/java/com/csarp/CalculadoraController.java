913b4291b273c057dd9d7a36106cbfbaf892cd53
csarp <csarp@DESKTOP-ANDOIPS>
2021-05-24 14:48:48 -0500

    submit v2

---
+++ src/main/java/com/csarp/CalculadoraController.java
@@ -0,0 +1,26 @@
+package com.csarp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller 
+public class CalculadoraController {
+	
+	@GetMapping("/calculadora")
+	public String index(Model modelo) {
+		 
+		modelo.addAttribute("calcu", new Calculadora());
+		return "/calculadora/index";
+	}
+	 
+	@PostMapping("/resultado")
+	public String resultado(@ModelAttribute Calculadora calcu, Model modelo) {
+		
+		calcu.Calcular();
+		modelo.addAttribute("calcu", calcu);
+		return "/calculadora/index";
+	} 
+}
